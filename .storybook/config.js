6fcbb19248aa0e3add4ef9fdb32147dc3c74dbdd
Patrick Fulton <fulton@heysparkbox.com>
2017-12-13 16:40:36 -0500

    initial commit

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,13 @@
-import { configure } from '@storybook/react';
+import React from 'react';
+import { configure, setAddon, addDecorator } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
 
 const req = require.context('../src/components', true, /.*story\.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+addDecorator((story, context) => withInfo()(story)(context));
 
 configure(loadStories, module);
